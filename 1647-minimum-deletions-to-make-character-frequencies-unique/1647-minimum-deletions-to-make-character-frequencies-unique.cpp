51dc0247a85574349e0bb170017e82d90c075153
KARAN CHHABRA <76587341+karankc23@users.noreply.github.com>
2022-06-28 13:45:15 +0530

    Time: 76 ms (65.20%), Space: 17.5 MB (37.46%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -6,47 +6,26 @@ public:
         for(auto &it:s){
             m[it-'a']++;
         }
-        unordered_map<int,int>mp;
-        int maxi = 0;
-        set<int>st;
-        for(int i = 0;i<=n;i++){
-            st.insert(i);
-        }
-        for(char c = 'a';c<='z';c++){
-            mp[m[c-'a']]++;
-            maxi = max(maxi,mp[m[c-'a']]);
-        }
-        // mp kehri frequency kinni vaar aa chukki
-        // _print(mp);
         vector<int>v;
-        for(auto &it:mp){
-            if(it.first==0){
-                continue;
-            }
-            if(it.second>1){
-                for(int i = 0;i<it.second-1;i++){
-                    v.push_back(it.first);
-                }
-                st.erase(it.first);
-            }
-            else if(it.second==1){
-                st.erase(it.first);
-            }
+        unordered_set<int>st;
+        for(auto it:m){
+            if(it!=0)
+            v.push_back(it);
         }
-        // _print(v);
-        // _print(st);
+        sort(v.begin(),v.end());
         int ans = 0;
-        st.insert(0);
-        for(auto &i:v){
-            auto it = --st.upper_bound(i);
-            int x = *it;
-            ans+=(i-x);
-            if(x!=0){
-                st.erase(it);
+        for(int i = 0 ;i<v.size();i++){
+            if(!st.count(v[i])){
+                st.insert(v[i]);
+            }
+            else{
+                while(st.count(v[i])){
+                    v[i]--;
+                    ans++;
+                }
+                if(v[i])st.insert(v[i]);
             }
         }
         return ans;
-
-
     }
 };
