4878eb87441b0911830ce581e0fb4bd92074e7d4
KARAN CHHABRA <76587341+karankc23@users.noreply.github.com>
2022-06-28 13:47:23 +0530

    Time: 96 ms (48.28%), Space: 17.3 MB (58.78%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int minDeletions(string s) {
-        int n = s.size();
+    int minDeletions(string st) {
+        int n = st.size();
         int m[26] = {0};
-        for(auto &it:s){
+        for(auto &it:st){
             m[it-'a']++;
         }
         vector<int>v;
-        unordered_set<int>st;
+        unordered_set<int>s;
         for(auto it:m){
             if(it!=0)
             v.push_back(it);
@@ -15,15 +15,15 @@ public:
         sort(v.begin(),v.end());
         int ans = 0;
         for(int i = 0 ;i<v.size();i++){
-            if(!st.count(v[i])){
-                st.insert(v[i]);
+            if(!s.count(v[i])){
+                s.insert(v[i]);
             }
             else{
-                while(st.count(v[i])){
+                while(s.count(v[i])){
                     v[i]--;
                     ans++;
                 }
-                if(v[i])st.insert(v[i]);
+                if(v[i])s.insert(v[i]);
             }
         }
         return ans;
