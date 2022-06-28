032993f788e001ae76cdad4cd2d5b9e2597409ca
KARAN CHHABRA <76587341+karankc23@users.noreply.github.com>
2022-06-28 12:15:14 +0530

    Time: 1762 ms (5.05%), Space: 194 MB (5.01%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        int n = s.size();
+        map<char,int>m;
+        for(auto &it:s){
+            m[it]++;
+        }
+        map<int,int>mp;
+        int maxi = 0;
+        set<int>st;
+        for(int i = 0;i<=n;i++){
+            st.insert(i);
+        }
+        for(char c = 'a';c<='z';c++){
+            mp[m[c]]++;
+            maxi = max(maxi,mp[m[c]]);
+        }
+        // mp kehri frequency kinni vaar aa chukki
+        // _print(mp);
+        vector<int>v;
+        for(auto &it:mp){
+            if(it.first==0){
+                continue;
+            }
+            if(it.second>1){
+                for(int i = 0;i<it.second-1;i++){
+                    v.push_back(it.first);
+                }
+                st.erase(it.first);
+            }
+            else if(it.second==1){
+                st.erase(it.first);
+            }
+        }
+        // _print(v);
+        // _print(st);
+        int ans = 0;
+        st.insert(0);
+        for(auto &i:v){
+            auto it = --st.upper_bound(i);
+            int x = *it;
+            ans+=(i-x);
+            if(x!=0){
+                st.erase(it);
+            }
+        }
+        return ans;
+
+
+    }
+};
