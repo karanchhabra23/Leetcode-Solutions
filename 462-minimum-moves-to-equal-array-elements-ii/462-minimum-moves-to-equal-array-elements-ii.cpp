d1a9a1ce2e6bef4b59b30dac3a4429c8b34f714d
KARAN CHHABRA <76587341+karankc23@users.noreply.github.com>
2022-07-01 00:28:22 +0530

    Time: 7 ms (97.98%), Space: 10.7 MB (99.77%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -8,23 +8,13 @@ public:
         if(n==2){
             return a[1]-a[0];
         }
-        if(n&1){
-            int mid = a[n/2];
-            long long sum = 0;
-            for(int i = 0;i<n;i++){
-                sum+= abs(a[i]-mid);
-            }
-            return sum;
+
+        int mid = a[n/2];
+        long long sum = 0;
+        for(int i = 0;i<n;i++){
+            sum+= abs(a[i]-mid);
         }
-        else {
-            int mid1 = a[n/2];
-            int mid2 = a[n/2 + 1];
-            long long sum1=0, sum2  = 0;
-            for(int i = 0;i<n;i++){
-                sum1+=abs(a[i]-mid1);
-                sum2+=abs(a[i]-mid2);
-            }
-            return min(sum1,sum2);
-        }
+        return sum;
+
     }
 };
