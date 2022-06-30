7392cc1060dbee87f010f4a66882da2c38b96c66
KARAN CHHABRA <76587341+karankc23@users.noreply.github.com>
2022-06-30 12:52:32 +0530

    Time: 736 ms (28.88%), Space: 185.7 MB (5.03%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,69 +1,42 @@
-class node{
-    public:
-    int key;
-    int value;
-    node* prev;
-    node* next;
-    node(int k, int v){
-        key = k;
-        value = v;
-    }
-    
-};
 class LRUCache {
 public:
-    node* head = new node(-1,-1);
-    node* tail = new node(-1,-1);
-    unordered_map<int,node*>m;
+    list<int>l;
+    unordered_map<int,list<int>::iterator>m;
+    unordered_map<int,int>mp;
     int c;
-    void del(node* n){
-        n->next->prev = n->prev;
-        n->prev->next = n->next;
-    }
-    
-    void add(node* n){
-        node* temp = head->next;
-        temp->prev = n;
-        n->next = temp;
-        head->next = n;
-        n->prev = head;
-    }
     
     LRUCache(int capacity) {
+        m.clear();
+        mp.clear();
         c = capacity;
-        head->next = tail;
-        tail->prev = head;
     }
     
-    int get(int k) {
-        if(m.find(k)!=m.end()){
-            node* temp = m[k];
-            int v = temp->value;
-            m.erase(k);
-            del(temp);
-            add(temp);
-            m[k] = head->next;
-            return v;
-        }
-        else{
+    int get(int key) {
+        if(mp.find(key)==mp.end())
             return -1;
-        }
+
+        l.erase(m[key]);
+        l.push_front(key);
+        m[key] = l.begin();
+        return mp[key];
     }
     
-    void put(int k, int v) {
-        if(m.find(k)!=m.end()){
-            node* exist = m[k];
-            m.erase(k);
-            del(exist);
+    void put(int key, int value) {
+        if(mp.count(key)){
+            l.erase(m[key]);
+            m.erase(key);
+            mp.erase(key);
         }
-        else if(m.size()==c){
-            node* temp = tail->prev;
-            m.erase(temp->key);
-            del(temp);
+        else if(mp.size()==c){
+            int x = l.back();
+            l.pop_back();
+            m.erase(x);
+            mp.erase(x);
         }
         
-        add(new node(k,v));
-        m[k] = head->next;
+        l.push_front(key);
+        mp[key] = value;
+        m[key] = l.begin();
     }
 };
 
