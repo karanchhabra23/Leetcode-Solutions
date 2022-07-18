40f379d5b7f79b055cb6bb58eb96ef72e67bf1a3
KARAN CHHABRA <76587341+karankc23@users.noreply.github.com>
2022-07-18 12:19:21 +0530

    Time: 77 ms (41.03%), Space: 6.6 MB (91.67%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     const int mod = 1e9 + 7;
     typedef long long ll;
-    ll dp[1010][1010];
+    // ll dp[1010][1010];
     int kInversePairs(int n, int k) {
         
         // memset(dp,-1,sizeof(dp));
@@ -10,24 +10,32 @@ public:
         if(n==1){
             return k==0;
         }
-        dp[2][0] = 1;
-        dp[2][1] = 1;
-        for(int i = 3;i<=n;i++){
-            dp[i][0]  = 1;
-            for(int j = 1;j<=k;j++){
-                if(j>(i)*(i-1)/2){
-                    continue;
-                }
-                dp[i][j] += dp[i][j-1];
-                dp[i][j] += dp[i-1][j] ;
-                if(j>=i){
-                    dp[i][j] +=  mod - dp[i-1][j-i];
-                }
-                dp[i][j]+=mod;
-                dp[i][j]%=mod;
-            }
+        if(k==0){
+            return 1;
         }
-        return dp[n][k];
+        vector<ll> curr(k+1,0),past(k+1,0);
+        // int past[k+1];
+        past[0] = 1;
+        past[1] = 1;
+        curr[0] = 1;
+        curr[1] = 1;
+        for(int i = 3;i<=n;i++){
+            curr[0]  = 1;
+            for(int j = 1;j<=min(k,i*(i-1)/2);j++){
+                // if(j>(i)*(i-1)/2){
+                //     continue;
+                // }
+                curr[j] = curr[j-1];
+                curr[j] += past[j] ;
+                if(j>=i){
+                    curr[j] +=  mod - past[j-i];
+                }
+                curr[j]+=mod;
+                curr[j]%=mod;
+            }
+            past = curr;
+        }
+        return past[k];
         
     }
 };
